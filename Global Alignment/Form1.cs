425eeb57491c7e8cec92e6cdcaf2ebde430dded6
Jan Kosiński <jan.grzegorz.kosinski@gmail.com>
2017-03-31 23:20:46 +0200

    Generator v5

--- Global Alignment/Form1.cs
+++ Global Alignment/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -19,8 +20,7 @@ namespace Global_Alignment
             InitializeComponent();
             dt = new DataTable();
             dt.Columns.Add(new DataColumn("Sequence ID", typeof(string)));
-            dt.Columns.Add(new DataColumn("Sequence", typeof(string)));
-            
+            dt.Columns.Add(new DataColumn("Sequence", typeof(string)));   
             dataGridView.DataSource = dt;
         }
 
@@ -40,21 +40,27 @@ namespace Global_Alignment
         {
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == DialogResult.OK) {
-                //TODO Fasta File validation
-                //TODO pozabezpieczac jak nie ma pliku itp itd
                 string path = ofd.FileName;
                 string temp = "";
-                string text = System.IO.File.ReadAllText(path);
+                string text = File.Exists(path) ? System.IO.File.ReadAllText(path) : "File doesn't exist error";    // reading from file; if file does not exist insert to text error message
+                if (text == "File doesn't exist error") { Console.WriteLine("File doesn't exist");  return;  }  // if file does not exist
+                Regex fastaRegex = new Regex(@"(>\S+\s+([atgcuryswkmbdhvnATGCURYSWKMBDHVN\s])+){2,}");  // fasta file validation. Fasta file must have at least 2 sequences
+                Match fastaMatch = fastaRegex.Match(text);
+                if (fastaMatch.Value.Trim() != text.Trim()) {
+                    Console.WriteLine("This is not valid fasta format!");
+                    return;
+                }
+                // end of fasta validation
                 Regex regex = new Regex(@">\S+\s+");
-                Match match = regex.Match(text);
-                string[] sequences = Regex.Split(text, @">\S+\s+");
-                sequences = sequences.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                Match match = regex.Match(text);    // taking sequence names from fasta
+                string[] sequences = Regex.Split(text, @">\S+\s+"); //taking sequences from fasta
+                sequences = sequences.Where(x => !string.IsNullOrEmpty(x)).ToArray();   //linq. Removing empty elements from array
                 List<string> names = new List<string>();
                 while (true)
                 {
                     if (match.Success)
                     {
-                        names.Add((match.Value).Trim());
+                        names.Add((match.Value).Trim());    // looking for sequence names as long as regex can find next one
                         match = match.NextMatch();
                     }
                     else
@@ -62,12 +68,12 @@ namespace Global_Alignment
                         break;
                     }
                 }
-                if (names.Count != sequences.Length) {
-                    Console.WriteLine("Wrong file format!");
-                    return;
+                if (names.Count != sequences.Length) {  // additional safety procedure. ?
+                    Console.WriteLine("Wrong file format!"); //?
+                    return; //?
                 }
                 for (int j = 0; j< names.Count; j++){
-                    this.dt.Rows.Add(new object[] { Regex.Replace(names[j], @"\s+", ""), Regex.Replace(sequences[j], @"\s+", "") });
+                    this.dt.Rows.Add(new object[] { Regex.Replace(names[j], @"\s+", ""), Regex.Replace(sequences[j], @"\s+", "") });    // adding rows to data table
                 }
 
 
